1e8c8a593e56e0994152c9be13d415680fd0120f
Prasanta Barman <prasanta.2001.7@gmail.com>
2020-03-16 10:00:49 +0530

    added simple voice recognition implementation

--- app/src/main/java/com/personalvoiceassistent/MainActivity.java
+++ app/src/main/java/com/personalvoiceassistent/MainActivity.java
@@ -1,14 +1,58 @@
 package com.personalvoiceassistent;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.resultText);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, String.format("onActivityResult: requestCode:%s, resultCode: %s, data:%s ", requestCode, resultCode, data));
+        if (resultCode == RESULT_OK && data != null) {
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (result != null) {
+                String input = result.get(0);
+                textView.setText(String.format("Result: %s", input));
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Failed to recognize speech!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void recogniseSpeech(View view) {
+        Log.d(TAG, "recogniseSpeech: ");
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(MainActivity.this, "Your Device Does Not support Speech Input", Toast.LENGTH_SHORT).show();
+        }
     }
 }
