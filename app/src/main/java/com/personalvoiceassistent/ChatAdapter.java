d09d5958d69ebbc3b6e072720fa825e2fd2bd90c
Prasanta Barman <prasanta.2001.7@gmail.com>
2020-03-17 17:09:29 +0530

    added basic java prototype code

---
+++ app/src/main/java/com/personalvoiceassistent/ChatAdapter.java
@@ -0,0 +1,68 @@
+package com.personalvoiceassistent;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.personalvoiceassistent.model.Chat;
+
+import java.util.ArrayList;
+
+
+class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> {
+    private static final String TAG = "MyAdapter";
+    private ArrayList<Chat> chats;
+
+    ChatAdapter(ArrayList<Chat> chats) {
+        Log.d(TAG, "MyAdapter: ");
+        this.chats = chats;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+
+        Chat chat = this.chats.get(viewType);
+        int layout = chat.isBot() ? R.layout.chat_bot : R.layout.chat_user;
+        RelativeLayout view = (RelativeLayout) layoutInflater.inflate(layout, parent, false);
+        Log.d(TAG, String.format("onCreateViewHolder: %s", viewType));
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return chats.get(position).getViewType();
+    }
+
+
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        TextView textView = holder.itemView.findViewById(R.id.txt);
+        Chat chat = chats.get(position);
+        textView.setText(chat.getMsg());
+        Log.d(TAG, String.format("onBindViewHolder: %s", position));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return chats.size();
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout layout;
+
+        MyViewHolder(@NonNull RelativeLayout itemView) {
+            super(itemView);
+            Log.d(TAG, "MyViewHolder: ");
+            layout = itemView;
+        }
+    }
+}
+
--- app/src/main/java/com/personalvoiceassistent/MainActivity.java
+++ app/src/main/java/com/personalvoiceassistent/MainActivity.java
@@ -1,15 +1,13 @@
 package com.personalvoiceassistent;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.personalvoiceassistent.model.Chat;
 
 import java.util.ArrayList;
 
@@ -17,7 +15,6 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private RecyclerView recyclerView;
     private ArrayList<Chat> chats = new ArrayList<>();
     private ChatAdapter chatAdapter;
 
@@ -27,13 +24,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
         chats.add(new Chat("hello", Chat.BOT));
         chats.add(new Chat("hi", Chat.USER));
         chats.add(new Chat("hello", Chat.BOT));
         chats.add(new Chat("hi", Chat.USER));
         chats.add(new Chat("hello", Chat.BOT));
-        chats.add(new Chat("hi", Chat.USER));
+        chats.add(new Chat("hasdi", Chat.USER));
 
 
         chatAdapter = new ChatAdapter(chats);
@@ -51,90 +48,3 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-class ChatAdapter extends RecyclerView.Adapter {
-    private static final String TAG = "MyAdapter";
-    private ArrayList<Chat> chats;
-
-    public ChatAdapter(ArrayList<Chat> chats) {
-        Log.d(TAG, "MyAdapter: ");
-        this.chats = chats;
-    }
-
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-
-        Chat chat = this.chats.get(viewType);
-        int layout = chat.isBot() ? R.layout.chat_bot : R.layout.chat_user;
-        View view = layoutInflater.inflate(layout, parent, false);
-        Log.d(TAG, String.format("onCreateViewHolder: %s", viewType));
-        return new MyViewHolder(view);
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return chats.get(position).getViewType();
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        Log.d(TAG, String.format("onBindViewHolder: %s", position));
-
-    }
-
-
-    @Override
-    public int getItemCount() {
-        return chats.size();
-    }
-
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        View layout;
-
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-            Log.d(TAG, "MyViewHolder: ");
-            layout = itemView;
-        }
-    }
-}
-
-
-class Chat {
-    static final int BOT = 0;
-    static final int USER = 1;
-    private String msg;
-    private int viewType;
-
-
-    public Chat(String msg, int viewType) {
-        this.msg = msg;
-        this.viewType = viewType;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public int getViewType() {
-        return viewType;
-    }
-
-    public void setViewType(int viewType) {
-        this.viewType = viewType;
-    }
-
-    public boolean isUser() {
-        return this.viewType == USER;
-    }
-
-    public boolean isBot() {
-        return this.viewType == BOT;
-    }
-
-}
